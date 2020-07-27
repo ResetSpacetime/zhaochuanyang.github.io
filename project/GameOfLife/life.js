8fc439f545daffc01607eb1e76c482c1d16b1d9d
arlmy <arlmy19@gmail.com>
2020-07-27 13:58:16 +0800

    Site updated: 2020-07-27 13:57:37

--- project/GameOfLife/life.js
+++ project/GameOfLife/life.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>window.onload = function() {
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
 
